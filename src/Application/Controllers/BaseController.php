d22f2eb083c9fcd08d72910d990cdb9e37dbbb43
Kirill Sakharov <mrcaxapov@gmail.com>
2023-07-19 17:46:35 +0300

    first commit

---
+++ src/Application/Controllers/BaseController.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+namespace Vozimsan\Core\Application\Controllers;
+
+class BaseController
+{
+    /**
+     * @return array<string, string>
+     */
+    public function actions(): array
+    {
+        return [];
+    }
+}
---
+++ src/Application/Router/Router.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+namespace Vozimsan\Core\Application\Router;
+
+class Router
+{
+    /**
+     * @return void
+     */
+    public static function init(): void
+    {
+
+    }
+}
